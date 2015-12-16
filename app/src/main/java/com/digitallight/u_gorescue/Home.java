6400a026a88df3888fe7980b85a0380cb203175a
Mochil <ilham.anugrah@mail.unpas.ac.id>
2015-12-16 11:29:43 +0700

    Hello Map

---
+++ app/src/main/java/com/digitallight/u_gorescue/Home.java
@@ -0,0 +1,17 @@
+package com.digitallight.u_gorescue;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+
+}
