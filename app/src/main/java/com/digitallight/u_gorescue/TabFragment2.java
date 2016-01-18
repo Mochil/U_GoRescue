1d8ee35211e5ed182d2437c56702ee6a15364c9f
Mochil <ilham.anugrah@mail.unpas.ac.id>
2016-01-19 00:13:54 +0700

    DeadLine Bro

--- app/src/main/java/com/digitallight/u_gorescue/TabFragment2.java
+++ app/src/main/java/com/digitallight/u_gorescue/TabFragment2.java
@@ -9,11 +9,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
 
 public class TabFragment2 extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_maps, container, false);
+
+        View Mmap = inflater.inflate(R.layout.activity_maps, container, false);
+        return Mmap;
     }
 }
